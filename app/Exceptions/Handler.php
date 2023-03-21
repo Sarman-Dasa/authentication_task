1eb19af23b44c8a79fd8d0954a09abbf2a1dc671
Sarman-Dasa <sarmand@zignuts.com>
2023-03-21 19:17:01 +0530

    handel error and store in errors table

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,13 @@
 
 namespace App\Exceptions;
 
+use App\Models\Error;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Auth;
+use PhpParser\Node\Stmt\TryCatch;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -22,7 +28,8 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+        // AuthorizationException::class,
+        // HttpException::class,
     ];
 
     /**
@@ -43,8 +50,42 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $exception, $request) {
+
+            $user_id = 1;
+
+            if (Auth::user()) {
+                $user_id = Auth::user()->id;
+            }
+
+            $data = array(
+                'user_id'   => $user_id,
+                'code'      => $exception->getCode(),
+                'file'      => $exception->getFile(),
+                'line'      => $exception->getLine(),
+                'message'   => $exception->getMessage(),
+                'trace'     => $exception->getTraceAsString(),
+            );
+            
+            Error::create($data);
+            
+            if($exception instanceof AuthenticationException)
+            {
+                return error("Can't access this page without Login!!!",type:'unauthenticated');
+            }
+           else if($exception instanceof RouteNotFoundException)
+            {
+                return error("Can't access this page without Login!!!",type:'unauthenticated');
+            }
+
+            else if($exception instanceof NotFoundHttpException)
+            {
+                return error(type:'notfound');
+            }
         });
     }
+
+
+    
+
 }
