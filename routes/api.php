d3c49ba3b431bf9c477c93c7574b66d175234bf2
Sarman-Dasa <sarmand@zignuts.com>
2023-03-21 19:27:27 +0530

    routes for auth and user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function()
+{
+    Route::post('register','register');
+    Route::get('/account-verify/{token}','verifyAccount');
+    Route::post('login','login');
+    Route::post('forgot-password','forgotPassword');
+    Route::post('reset-password','resetPassword');
 });
+
+Route::middleware(['auth:api'])->group(function(){
+    Route::controller(UserController::class)->group(function(){
+        Route::post('list','list');
+        Route::get('get/{id}','get');
+        Route::get('logout','logout');
+        Route::post('change-password','changePassword');
+    });
+});
+
