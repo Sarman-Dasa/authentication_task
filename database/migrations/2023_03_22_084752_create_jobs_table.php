bc52e091c8deeeeb0bf5a5acfcca25fc39050ed0
Sarman-Dasa <sarmand@zignuts.com>
2023-03-22 19:30:31 +0530

    create job table for crud operation

---
+++ database/migrations/2023_03_22_084752_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('type',150);
+            $table->integer('vacancy');
+            $table->unsignedBigInteger('company_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
