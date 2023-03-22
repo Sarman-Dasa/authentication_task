b2e30185c382ad815331af9360789573f3961ee9
Sarman-Dasa <sarmand@zignuts.com>
2023-03-22 19:30:11 +0530

    create employees table for crud operation

---
+++ database/migrations/2023_03_22_072453_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',30);
+            $table->string('last_name',30);
+            $table->string('email',150)->unique();
+            $table->bigInteger('phone')->unique();
+            $table->date('joining_date');
+            $table->unsignedBigInteger('company_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
