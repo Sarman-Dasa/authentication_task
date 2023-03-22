3cf20407ec2df022530b97714f6b7c560eaed6f0
Sarman-Dasa <sarmand@zignuts.com>
2023-03-22 19:27:11 +0530

    add fillable column name and softDelete

---
+++ app/Models/Job.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Job extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['type' ,'vacancy' ,'company_id'];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
