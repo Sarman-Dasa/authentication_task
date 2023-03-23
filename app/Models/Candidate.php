f804d51f0aceb6fa27f0671d4d3720f25a60d609
Sarman-Dasa <sarmand@zignuts.com>
2023-03-23 19:29:10 +0530

    add relationship with job

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -10,4 +10,12 @@ class Candidate extends Model
     use HasFactory;
 
     protected $fillable = ['first_name' , 'last_name' ,'email' ,'phone' ,'resume' ,'job_id'];
+
+    /**
+     * Candidate-Job Relation
+     */
+    public function job()
+    {
+        return $this->belongsTo(Job::class,'job_id','id')->with('company');
+    }
 }
