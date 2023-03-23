0c84a18e0abc5cd5eff6093766f87671b7f20d0f
Sarman-Dasa <sarmand@zignuts.com>
2023-03-23 15:38:30 +0530

    add fillable column name

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['first_name' , 'last_name' ,'email' ,'phone' ,'resume' ,'job_id'];
 }
