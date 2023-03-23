d4b85337643bb357027b9d6cd2021143d3fd8fb8
Sarman-Dasa <sarmand@zignuts.com>
2023-03-23 19:27:54 +0530

    export employee data

--- app/Exports/ExportEmployee.php
+++ app/Exports/ExportEmployee.php
@@ -3,16 +3,30 @@
 namespace App\Exports;
 
 use App\Models\Employee;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportEmployee implements FromCollection
+
+class ExportEmployee implements FromCollection , WithHeadings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+
+    public function __construct($start_date,$end_date)
+    {
+        $this->start_date = $start_date;
+        $this->end_date   = $end_date;
+    }
+
     public function collection()
     {
-        //dd("done");
-        return Employee::all();
+        return Employee::whereBetween('joining_date',[$this->start_date,$this->end_date])->get();
     }
+
+    public function headings() :array
+    {
+        return ['ID','First Name' , 'Last Name' ,'Email' ,'Phone' ,'Joining Date' ,'Company Id'];
+    }
+
+
 }
